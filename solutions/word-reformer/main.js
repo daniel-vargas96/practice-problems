aac2bdb54e61fb048684a0da8a85731688188452
Daniel Vargas <dannyvargashb@gmail.com>
2020-05-28 10:23:45 -0700

    Completed word-reformer

---
+++ solutions/word-reformer/main.js
@@ -0,0 +1,6 @@
+function wordReformer(wordFragment1, wordFragment2) {
+  return wordFragment1 + wordFragment2;
+}
+
+var wordReformerResult = wordReformer('bro', 'ken');
+console.log('wordReformer function:', wordReformerResult);
