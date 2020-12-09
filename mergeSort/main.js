b35eb346ff63ec8bda115a2b47b760fc0300e003
Daniel Vargas <dannyvargashb@gmail.com>
2020-12-09 14:06:10 -0800

    merging two sorted arrays

--- mergeSort/main.js
+++ mergeSort/main.js
@@ -1,6 +1,25 @@
 function mergeSort(arr1, arr2) {
   let output = [];
-  let i;
-  let j;
-  while()
+  let i = 0;
+  let j = 0;
+  while(i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+        output.push(arr1[i])
+        i++;
+    } else {
+      output.push(arr2[j])
+      j++;
+    }
+  }
+  while(i < arr1.length) {
+    output.push(arr1[i])
+    i++;
+  }
+  while (j < arr2.length) {
+    output.push(arr2[j])
+    j++;
+  }
+  return output;
 }
+
+console.log(mergeSort([1,3,5],[20,45,46]))
